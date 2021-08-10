0d93bc88eefc65d74d222a430879c42278bb2644
Muhammad-VII <meshoronaldo62@gmail.com>
2021-08-10 15:48:21 +0200

    1.1.0

--- modules/users/controller/users.controller.js
+++ modules/users/controller/users.controller.js
@@ -1,16 +1,20 @@
+const fs = require('fs')
+const path = require('path')
 // ** DB ** //
-let users = [
-  { id: 1, name: "Muhammad Akmal", age: 21 },
-  { id: 2, name: "Ali Mahmoud", age: 23 },
-  { id: 3, name: "Ahmed Magdy", age: 24 },
-  { id: 4, name: "Samar Selim", age: 30 },
-  { id: 5, name: "Heba Ahmed", age: 25 },
-];
+// let users = [
+//   { id: 1, name: "Muhammad Akmal", age: 21 },
+//   { id: 2, name: "Ali Mahmoud", age: 23 },
+//   { id: 3, name: "Ahmed Magdy", age: 24 },
+//   { id: 4, name: "Samar Selim", age: 30 },
+//   { id: 5, name: "Heba Ahmed", age: 25 },
+// ];
 // ** End ** //
 
 //** Get all users **//
 const getAllUsers = (req, res) => {
-  res.json(users);
+  let data = fs.readFileSync(path.join(__dirname, "../db/db.json"))
+  let parsedData = JSON.parse(data)
+  res.json(parsedData);
 };
 
 const addUser = (req, res) => {
